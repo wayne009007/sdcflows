1c8aef72d68c85f059f659e303cd5350b44e8652
Documentation Push <crn.poldracklab@gmail.com>
2019-11-20 02:43:57 +0000

    doc(update) [skip ci]

--- api/sdcflows-workflows-syn-1.py
+++ api/sdcflows-workflows-syn-1.py
@@ -1,4 +1,4 @@
 from sdcflows.workflows.syn import init_syn_sdc_wf
 wf = init_syn_sdc_wf(
-    bold_pe='j',
+    epi_pe='j',
     omp_nthreads=8)
--- api/sdcflows-workflows-unwarp-2.py
+++ api/sdcflows-workflows-unwarp-2.py
@@ -1,4 +1,3 @@
 from sdcflows.workflows.unwarp import init_sdc_unwarp_wf
 wf = init_sdc_unwarp_wf(omp_nthreads=8,
-                        fmap_demean=True,
                         debug=False)
