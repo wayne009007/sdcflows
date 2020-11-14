fc3d62b889f8bd3d7ceaec775e494707344f99c6
Documentation Push <nipreps@gmail.com>
2020-11-14 13:54:26 +0000

    doc(update) [skip ci]

---
+++ api/sdcflows-models-fieldmap-1.py
@@ -0,0 +1,2 @@
+from sdcflows.models.fieldmap import init_fmap_postproc_wf
+wf = init_fmap_postproc_wf(omp_nthreads=6)
---
+++ api/sdcflows-models-fieldmap-4.py
@@ -0,0 +1,2 @@
+from sdcflows.models.fieldmap import init_phdiff_wf
+wf = init_phdiff_wf(omp_nthreads=1)
