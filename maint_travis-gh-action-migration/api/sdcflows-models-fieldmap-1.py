fe82273591280f0ee0b1556d6edbc149476afc72
Documentation Push <nipreps@gmail.com>
2020-12-09 21:15:48 +0000

    doc(update) [skip ci]

--- maint_travis-gh-action-migration/api/sdcflows-models-fieldmap-1.py
+++
@@ -1,2 +0,0 @@
-from sdcflows.models.fieldmap import init_fmap_postproc_wf
-wf = init_fmap_postproc_wf(omp_nthreads=6)
--- maint_travis-gh-action-migration/api/sdcflows-models-pepolar-1.py
+++
@@ -1,2 +0,0 @@
-from sdcflows.models.pepolar import init_3dQwarp_wf
-wf = init_3dQwarp_wf()
